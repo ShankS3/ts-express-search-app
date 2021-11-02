9494ca6f9ac5309286ebb8793cf07528e8e699b4
ShankS3 <shashank.surwase@gmail.com>
2021-11-03 01:59:44 +0530

    Added test case to get response on /feeds

---
+++ src/controllers/__tests__/feeds.test.ts
@@ -0,0 +1,38 @@
+import request from "supertest";
+import createServer from "server";
+
+const app = createServer();
+
+const expected = [
+  {
+    "name": "Customer Assurance Liaison",
+    "image": "http://lorempixel.com/640/480",
+    "description": "Vel voluptatem id repudiandae aut omnis. Deleniti tempore aliquam quia magnam eos. Sunt saepe nisi delectus.",
+    "dateLastEdited": "2018-05-19T12:33:25.545Z"
+  },
+  {
+    "name": "Dynamic Infrastructure Designer",
+    "image": "http://lorempixel.com/640/480",
+    "description": "Quaerat in rerum. Possimus reprehenderit provident ea voluptatem qui et enim. Ducimus ea soluta esse modi quia.",
+    "dateLastEdited": "2017-11-28T04:59:13.759Z"
+  },
+  {
+    "name": "Regional Configuration Designer",
+    "image": "http://lorempixel.com/640/480",
+    "description": "Rerum voluptatibus deleniti. Et quo ea magnam quisquam aliquam sequi sed praesentium. Similique est maiores. Tempora sed ad dolores error deserunt possimus sed perferendis molestiae. Doloribus fuga velit ipsum voluptatem ut ducimus.",
+    "dateLastEdited": "2018-07-27T21:33:53.485Z"
+  },
+];
+
+describe('GET /feeds', () => {
+  it('should return 200 & valid response if only pageNo provided', (done) => {
+    request(app)
+      .get('/feeds')
+      .expect(200)
+      .end((err, res) => {
+          if(err) return done(err);
+          expect(res.body).toEqual(expected);
+          done();
+      });
+  });
+});
