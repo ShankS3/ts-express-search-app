42706cf3aed6aa33112602953c5df0954572d664
ShankS3 <shashank.surwase@gmail.com>
2021-11-03 15:23:18 +0530

    Added test to check bad request

--- src/routes/__tests__/feeds.test.ts
+++ src/routes/__tests__/feeds.test.ts
@@ -4,7 +4,15 @@ import createServer from "server";
 const app = createServer();
 
 describe("feed routes", () => {
-    it("/feeds should respond with 200", (done) => {
-        request(app).get("/feeds").expect(200, done);
-    });
+  it("/feeds should respond with 200", async () => {
+    await request(app)
+      .get("/feeds?page=1&searchText=&sortBy=")
+      .expect(200);
+  });
+
+  it("/feeds should respond with 400 if no page param found", async () => {
+    let response = await request(app).get("/feeds");
+    expect(response.status).toEqual(400);
+    expect(response.text).toEqual("Required parameter 'page' is missing");
+  })
 });
