43324794be2011b928ba878b7b190feb56b03116
ShankS3 <shashank.surwase@gmail.com>
2021-11-03 17:11:04 +0530

    Updated query validator to pass test

--- src/utility/valiateQueryParams.ts
+++ src/utility/valiateQueryParams.ts
@@ -5,7 +5,12 @@ const validateQueryParams = (params: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     for(const param of params) {
         if(isEmpty(req.query[param]))
-          return res.status(400).send(`Required parameter '${param}' is missing`);
+          return res
+                  .status(400)
+                  .send({
+                    name: "Bad Request", 
+                    message: `Required parameter '${param}' is missing`
+                  });
     }
     next();
   };
