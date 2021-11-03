253b476a775a286ef251b325f33c98854f61959d
ShankS3 <shashank.surwase@gmail.com>
2021-11-03 16:54:33 +0530

    Added search logic in feeds controller

--- src/controllers/feeds.ts
+++ src/controllers/feeds.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { query, Request, Response } from 'express';
 import fs from 'fs';
 import _ from 'lodash';
 import path from 'path';
@@ -7,17 +7,20 @@ import { Feed, FeedsResponse } from 'models/feeds';
 const page_size: number = 10;
 
 export const getFeeds = async(req: Request, res: Response): Promise<void> => {
-  const { page, searchText="", sortBy="" } = req.query;
-  let reqPage = parseInt(<string>page,10);
-  let [sortOn, sortType] = _.split(<string>sortBy, ',');
+  const { page, searchText="", sortBy="" } = req.query as { [key: string]: string};
+  let reqPage = parseInt(page,10);
+  let [sortOn, sortType] = _.split(sortBy, ',');
+
+  const quotedText = /^"(.+)"$/;
+  let searchString: string[] = (quotedText.test(searchText)) ? [searchText.slice(1, searchText.length-1)] : searchText.split(" ");
 
   let rawData = fs.readFileSync(path.resolve('public/assets', 'feeds.json'));
   let feeds: Feed[] = !_.isEmpty(rawData) ? JSON.parse(rawData.toString()) : [];
-  feeds.map(feed => new Date(feed.dateLastEdited));
   
   let data: Feed[] = _.chain(feeds)
-                      .filter(feed => _.includes(feed.name, searchText)
-                                      || _.includes(feed.description, searchText)
+                      .filter(feed => 
+                        searchString.every(item => _.includes(_.toLower(feed.name), _.toLower(item))) 
+                        || searchString.every(item => _.includes(_.toLower(feed.description), _.toLower(item)))
                       )
                       .orderBy([sortOn], [<'asc'|'desc'>sortType])
                       .slice(page_size*(reqPage-1), page_size*reqPage)
