95f1e4be21202cb9399d9fc64b0c9d4caa3183bf
ShankS3 <shashank.surwase@gmail.com>
2021-11-03 18:48:26 +0530

    Added cors header

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import feeds  from "routes/feeds";
+import cors from 'cors';
 
 
 export default function createServer() {
   const app: Application = express();
 
+  app.use(cors());
   app.use("/feeds", feeds);
 
   app.get("/", (err: any, req: Request, res: Response, next: NextFunction) => {
